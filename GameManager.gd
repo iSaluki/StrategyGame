a2e797b50363f529abc7967192127afae1fa75a9
Seth <sethmb@protonmail.com>
2022-04-24 20:59:57 +0100

    Resource cleanups

--- GameManager.gd
+++ GameManager.gd
@@ -1,7 +1,6 @@
 extends Control
 
 # Base vars
-export var resources = 80
 export var population = 5
 export var military = 0
 export var countries = ["Spain", "England", "France", "USA"]
@@ -13,19 +12,20 @@ var cityIndex = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GlobalData.resources += 50
 	match country:
 		"Spain":
-			resources += 10
+			GlobalData.resources += 10
 			population += 1
 			military += 5
 			cities = ["Madrid", "Barcelona", "Seville", "Bilbao", "Valencia", "Granada"]
 		"England":
-			resources += 50
+			GlobalData.resources += 50
 			population -= 1
 			military += 10
 			cities = ["London", "Birmingham", "Manchester", "Hull", "Worcester", "Gloucester"]
 		"France":
-			resources += 5
+			GlobalData.resources += 5
 			population += 1
 			military += 2
 			cities = ["Paris", "Calais", "Bordeaux", "Dunkirk", "Dijon", "Mulhouse"]
@@ -33,26 +33,31 @@ func _ready():
 			population += 10
 			military += 20
 			cities = ["Washington", "New York", "San Francisco", "Detroit", "Chicago"]
-	$CitiesLabel/CityContainer/VSplitContainer/City0/Name.set_text(cities[0])
+	
+	# Setup capital
+	
+	$CitiesLabel/CityContainer/VSplitContainer/City0.cityName = str(cities[0])
 	$CitiesLabel/CityContainer/VSplitContainer/City0.toggleActive(true)
 	
 	
 func _process(delta):
-	#$ResourcesLabel.set_text("Resources: "+ str(resources)) -- Obsolete, moved to city script
-	#$PopulationLabel.set_text("Population: "+ str(population)) -- Obsolete, moved to city script
+	#$ResourcesLabel.set_text("Resources: "+ str(resources)) -- Obsolete, moved to tick
+	#$PopulationLabel.set_text("Population: "+ str(population)) -- Obsolete, moved to tick + city script
 	$MilitaryLabel.set_text("Military: "+ str(military))
 
 
 func _on_NewCityBtn_pressed():
-	if resources >= 40 and cityIndex !=4:
+	if GlobalData.resources >= 40 and cityIndex !=4:
 		# Create city
-		resources -= 40
+		GlobalData.resources -= 40
 		var newCity = get_node("/root/Game/CitiesLabel/CityContainer/VSplitContainer/City"+str(cityIndex))
+		newCity.cityName = cities[cityIndex]
 		newCity.toggleActive(true)
 		cityIndex += 1
 
 
 func _on_TickTimer_timeout():
+	var totalPop = 0
 	for c in cityIndex:
 		var city = get_node("/root/Game/CitiesLabel/CityContainer/VSplitContainer/City"+str(c))
 		# Population growth
@@ -61,11 +66,12 @@ func _on_TickTimer_timeout():
 			pass
 		if randVal < 0.2:
 			city.pop += 1
-		
+		totalPop += city.pop
 		# Resource production
 		randVal = randf()
 		if randVal < 0.2:
 			pass
 		if randVal < 0.8:
-			resources += city.prod * city.pop
-		
+			GlobalData.resources += city.prod * city.pop
+		$ResourcesLabel.set_text("Resources: "+ str(GlobalData.resources))
+	$PopulationLabel.set_text("Population: "+ str(totalPop))
--- city.gd
+++ city.gd
@@ -7,6 +7,12 @@ export var cityID = -1
 export var pop = 1
 export var prod = 1
 
+var cityName = ""
+
+
+var upgradeScale = 1
+var baseUpPrice = 50
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -14,6 +20,7 @@ func _ready():
 
 func toggleActive(state):
 	if state == true:
+		$Name.set_text(cityName)
 		self.show()
 	if state == false:
 		self.hide()
@@ -21,3 +28,12 @@ func toggleActive(state):
 func _process(delta):
 	$Pop.set_text("Population: "+ str(pop))
 	$Prod.set_text("Production: "+ str(prod))
+
+
+func _on_Button_pressed():
+	if GlobalData.resources >= upgradeScale * baseUpPrice:
+		prod += 1
+		GlobalData.resources -= upgradeScale * baseUpPrice
+		
+		upgradeScale += rand_range(0.1,1.0)
+		
