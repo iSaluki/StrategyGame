7bbd2f5108c9679db4116ab9af6a70d77172b28a
Seth <sethmb@protonmail.com>
2022-04-24 13:29:28 +0100

    Add military system

--- GameManager.gd
+++ GameManager.gd
@@ -3,6 +3,7 @@ extends Control
 # Base vars
 export var resources = 80
 export var population = 5
+export var military = 0
 export var countries = ["Spain", "England", "France", "USA"]
 var country = countries[0]
 var cities = []
@@ -17,17 +18,21 @@ func _ready():
 		"Spain":
 			resources += 10
 			population += 1
+			military += 5
 			cities = ["Madrid", "Barcelona", "Seville", "Bilbao", "Valencia", "Granada"]
 		"England":
 			resources += 50
 			population -= 1
+			military += 10
 			cities = ["London", "Birmingham", "Manchester", "Hull", "Worcester", "Gloucester"]
 		"France":
 			resources += 5
 			population += 1
+			military += 2
 			cities = ["Paris", "Calais", "Bordeaux", "Dunkirk", "Dijon", "Mulhouse"]
 		"USA":
 			population += 10
+			military += 20
 			cities = ["Washington", "New York", "San Francisco", "Detroit", "Chicago"]
 	$CitiesLabel/CityContainer/VSplitContainer/CapitalCity.set_text(cities[0])
 	
@@ -35,6 +40,7 @@ func _ready():
 func _process(delta):
 	$ResourcesLabel.set_text("Resources: "+ str(resources))
 	$PopulationLabel.set_text("Population: "+ str(population))
+	$MilitaryLabel.set_text("Military: "+ str(military))
 
 # FIXME: When more than two cities are made, the system breaks and they begin overlapping
 func _on_NewCityBtn_pressed():
